31c309572734b11b6cbbad6aadf1e64a4b65dfee
Satyam <nikamsatyam187@gmail.com>
2025-12-16 11:01:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
 
+// Functional React Component
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div>
+      {/* The required text inside a <p> tag */}
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+};
 
 export default App;
